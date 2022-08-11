46f36abbf7dd141207de241e2a73b15855473b4d
Pete Fowler <fowler.pete@gmail.com>
2022-08-11 17:48:36 -0600

    commit tree 774f05714da727adb84e3548e391634aa8b89442

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+
+
+function submitData(user, email) {
+  const body = document.querySelector('body');
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({"name": user, "email": email}),
+  })
+  .then(res => res.json())
+  .then(data => body.append(data.id))
+  .catch(err => body.append(`Problem: ${err.message}!`));
+}
+
+submitData('pete', 'fowler.pete@gmail.com');
